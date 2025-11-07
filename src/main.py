066f0f3360a1d0f36361ea32e5b7245fb8dadbda
jo17-dev <procompter1@gmail.com>
2025-11-07 11:15:53 -0500

    Ajout des séparations en fonctions

--- src/main.py
+++ src/main.py
@@ -1,9 +1,24 @@
 # fichier pour handle toutes les commandes
 from sys import argv
 from helpers.constantes import CLI_PARAMS as allowed_params
-from presentation.cli import display_help
+from presentation.cli import display_help, ajouter_note_cli, ajouter_note_editeur, edit_note_editeur, afficher_note_cli, mauvais_params
 
-if(argv[1] not in allowed_params):
-    print("Bad params")
-    display_help()
-
+if(len(argv) == 1):
+    # cas sans parametre
+    afficher_note_cli(argv)
+else:
+    match argv[1]:
+        case "-a":
+            ajouter_note_cli()
+        case "-af":
+            ajouter_note_editeur(argv[1:])
+        case "-ef":
+            edit_note_editeur(argv[1:])
+        case "-s":
+            afficher_note_cli(argv[1:])
+        case "-sd":
+            afficher_note_cli(argv[1:])
+        case "--help":
+            display_help()
+        case _:
+            mauvais_params()
--- src/presentation/cli.py
+++ src/presentation/cli.py
@@ -11,3 +11,19 @@ def display_help():
     print("---- Done by jo17-dev -----")
     print("---- Hope it helped -----")
     
+
+def ajouter_note_cli(arg: list):
+    print("Option -a : Ajouter une note:: " +arg)
+
+def ajouter_note_editeur(arg: list):
+    print("Option -af : Ajouter note en mode fichier")
+    print(arg)
+
+
+def edit_note_editeur(arg: list):
+    print("Option -ef : editer en mode fichier")
+    print(arg)
+
+def afficher_note_cli(arg: list):
+    print("Option -s : afficher en mode cli les notes d'aujourdhui")
+    print(arg)
